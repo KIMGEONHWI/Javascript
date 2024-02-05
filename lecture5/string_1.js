91e9e825680e69dedc531ff616031a599b3d0632
김건휘 <66071954+KIMGEONHWI@users.noreply.github.com>
2024-02-05 21:26:35 +0900

    string type

---
+++ lecture5/string_1.js
@@ -0,0 +1,5 @@
+let str = "abc"; //큰따옴표
+let str2 = "def"; //작은따옴표
+//시작과 끝 따옴표 일치 시키기!
+
+console.log(str, str2);
---
+++ lecture5/string_3.js
@@ -0,0 +1,3 @@
+//I'm Fine Thank You! "and you?" -> 큰 따옴표 작은 따옴표 동시에 있을 때
+let str = "I'm Fine Thank You! \"and you?\""; //역슬래시 이스케이프 문자로 해결
+console.log(str);
