e4f7b62ddda1c1a3466b4fa05a3972bb416bccf1
김건휘 <66071954+KIMGEONHWI@users.noreply.github.com>
2024-02-06 21:17:35 +0900

    undefined, null type

---
+++ lecture6/null.js
@@ -0,0 +1,4 @@
+//null은 사용자가 명시적으로 변수 공간이 비어있음을 의미할 때 사용
+let num = null; //사용자가 의미적으로 공간을 비워놈
+console.log(num);
+//null 출력
---
+++ lecture6/undefined.js
@@ -0,0 +1,4 @@
+//undefined는 자바스크립트가 자동으로 부여해주는 값(데이터 할당 전까지)
+let num;
+console.log(num);
+//undefined 출력
