4b832e0aa4371e647bbdaa7630b2b5925a98c1ca
김건휘 <66071954+KIMGEONHWI@users.noreply.github.com>
2024-02-07 18:47:51 +0900

    operator

---
+++ lecture14/operator_1.js
@@ -0,0 +1,21 @@
+//덧셈 연산자
+let addition = 20 + 10;
+console.log(addition);
+
+//뺄셈 연산자
+let subtraction = 20 - 10;
+console.log(subtraction)
+
+//곱셈 연산자
+let muiltiplication = 10 * 10;
+console.log(muiltiplication);
+
+//나눗셈 연산자
+let division = 10 / 5;
+console.log(division);
+
+//나머지 연산자
+let remainder = 10 % 3;
+console.log(remainder);
+
+//산순연산자
---
+++ lecture14/operator_3.js
@@ -0,0 +1,15 @@
+//숫자와 문자를 더할 경우
+let changeType1 = 20 + '10'; //덧셈만 문자열 연결로 처리됨.(2010 출력)
+console.log(changeType1);
+
+//숫자와 문자를 뺀 경우
+let changeType2 = 20 - '10';
+console.log(changeType2);
+
+//숫자와 문자를 곱한 경우
+let changeType3 = 20 * '10';
+console.log(changeType3);
+
+//숫자와 문자를 나눌 경우
+let changeType4 = 20 / '10';
+console.log(changeType4);
