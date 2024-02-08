35c541918a82619960b544c0f1f06dff57d661db
김건휘 <66071954+KIMGEONHWI@users.noreply.github.com>
2024-02-08 15:51:29 +0900

    comparison operator

---
+++ lecture16/comparison-operator-2.js
@@ -0,0 +1,15 @@
+//동등(==)
+let comparison1 = 10 == "10"; //문자열 10을 숫자형 10으로 자동으로 형변환해서 비교연산 수행
+console.log(comparison1);
+
+//일치(===)
+let comparison2 = 10 === "10"; //자료형까지 일치하는지 확인
+console.log(comparison2);
+
+//부등(!=)
+let comparison3 = 10 != "10";
+console.log(comparison3);
+
+//불일치(!==)
+let comparison4 = 10 !== "10";
+console.log(comparison4);
